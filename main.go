9631bd58d7b422156229ec898bd184f851fcc533
Chiraz-Hamadi-Piriou <chiraz.hamadi-piriou@outscale.com>
2022-05-31 15:14:48 +0200

    Implementing logs to a file

--- main.go
+++ main.go
@@ -16,14 +16,11 @@ import (
 func displayLogs(args []string, options map[string]string) int {
 	config := osc.NewConfiguration()
 	config.Debug = false
-
 	client := osc.NewAPIClient(config)
-
 	ctx := context.WithValue(context.Background(), osc.ContextAWSv4, osc.AWSv4{
 		AccessKey: os.Getenv("OSC_ACCESS_KEY"),
 		SecretKey: os.Getenv("OSC_SECRET_KEY"),
 	})
-
 	logDate := time.Now().UTC().Format("2006-01-02T15:04:05")
 	stopSignal := make(chan os.Signal, 1)
 	signal.Notify(stopSignal, syscall.SIGINT)
@@ -35,7 +32,6 @@ func displayLogs(args []string, options map[string]string) int {
 				QueryDateAfter: &logDate,
 			},
 		}
-
 		resp, httpRes, err := client.ApiLogApi.ReadApiLogs(ctx).ReadApiLogsRequest(req).Execute()
 		if err != nil {
 			fmt.Printf("Error %v", err)
@@ -48,33 +44,47 @@ func displayLogs(args []string, options map[string]string) int {
 		if !resp.HasLogs() || len(logs) == 0 {
 			continue
 		}
+		var file *os.File
+		var fileError error
+		lineBreak := []byte("\n")
+		if options["write"] != "" {
+			file, fileError = os.OpenFile(options["write"], os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+			if fileError != nil {
+				fmt.Fprintln(os.Stderr, "Error: can not open the file")
+			}
+			defer file.Close()
 
+		}
 		for _, log := range logs {
 			jsonLog, marshalError := json.Marshal(log)
 			if marshalError != nil {
 				fmt.Fprintf(os.Stderr, "Error: can not read log output")
 				return 1
 			}
-			fmt.Println(string(jsonLog))
-
+			if file != nil {
+				logWriting := []byte(string(jsonLog))
+				file.Write(logWriting)
+				file.Write(lineBreak)
+			} else {
+				fmt.Println(string(jsonLog))
+			}
 		}
-
 		lastLog := logs[len(logs)-1]
 		logDate = *lastLog.QueryDate
-
 		go func() {
 			<-stopSignal
 			os.Exit(0)
 		}()
-
 	}
 	return 0
-
 }
-
+func AddWriteOption() cli.Option {
+	return cli.NewOption("write", "Write all traces inside a file instead of writing to standard output").WithChar('w').WithType(cli.TypeString)
+}
 func main() {
 	app := cli.New("osc-logs").
-		WithAction(displayLogs)
+		WithAction(displayLogs).
+		WithOption(AddWriteOption())
 	ret := app.Run(os.Args, os.Stdout)
 	os.Exit(ret)
 }
